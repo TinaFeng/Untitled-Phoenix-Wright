7ff140be568e6ce196c33eabdd75bc6b1ba8c6cc
Lawrence DAng <reblank.et@gmail.com>
2018-04-29 14:21:42 -0700

    Added basic audio manager.

---
+++ Untitled Phoenix Wright/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,230 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+/* Class to play various audio clips such as typing sounds,
+ * sound effects, and background music.
+ */
+
+//TODO: Associate tags with clips/file names from external file.
+//TODO: Test it.
+//TODO: Audio options (volume levels, etc.)
+
+public class AudioManager : MonoBehaviour
+{
+    //AudioSources
+    //AudioSource for typing sounds
+    public AudioSource type_source;
+    //AudioSource for various scripted sound effects
+    public AudioSource sfx_source;
+    //AudioSource for background music
+    public AudioSource bgm_source;
+    //AudioSource for sound effects related to UI interactions (e.g. button presses, clicking to progress dialogue, etc.)
+    public AudioSource ui_source;
+
+    //Dictionaries associating AudioClips and sound file names to tags
+
+    /* Dictionary containing typing AudioClips
+     * Current typing sound tags:
+     * "male": TypingMale.wav
+     * "female": TypingFemale.wav
+     * "neutral": Typewriter.wav
+     */
+    Dictionary<string, AudioClip> voices = new Dictionary<string, AudioClip>();
+
+    /* Dictionary containing file names and tags for sound effects.
+     * Current sound effect tags:
+     * "shock": Test_Shock.wav
+     * "select": Test_Select.wav
+     */
+    Dictionary<string, string> sfx_dict = new Dictionary<string, string>();
+
+    /* Dictionary containing file names and tags for BGM
+     * Current background music tags:
+     * "lobby": Court
+     */
+    Dictionary<string, string> bgm_dict = new Dictionary<string, string>();
+    //AudioClip typing;
+
+    //Hold the tag of the last assigned sound to the AudioSources.
+    //Used to avoid redundant loading of sound files.
+    string current_sfx = "";
+    string current_bgm = "";
+    string current_ui = "";
+
+    //Determines whether the background music should be played.
+    bool bgm_playing = false;
+
+    // Intitialization
+    void Awake()
+    {
+        //Load typing sounds
+        voices["male"] = Resources.Load("TypingMale") as AudioClip;
+        voices["female"] = Resources.Load("TypingFemale") as AudioClip;
+        voices["neutral"] = Resources.Load("Typewriter") as AudioClip;
+
+        //Load sound effect file names
+        sfx_dict["shock"] = "Test_Shock";
+        sfx_dict["select"] = "Test_Select";
+        //Load bgm file names
+        bgm_dict["lobby"] = "Court_Lobby";
+
+        //Initialize AudioSources
+        type_source = gameObject.AddComponent<AudioSource>() as AudioSource;
+        sfx_source = gameObject.AddComponent<AudioSource>() as AudioSource;
+        bgm_source = gameObject.AddComponent<AudioSource>() as AudioSource;
+        ui_source = gameObject.AddComponent<AudioSource>() as AudioSource;
+
+        bgm_source.loop = true; //bgm loops by default; other sounds probably shouldn't
+
+        //Set default values for AudioSources
+        SetUISound("select");
+        SetTypeSound("neutral"); //default typing sound
+        SetBGM("lobby");
+        type_source.volume = 0.4f;
+        sfx_source.volume = 1.0f;
+        ui_source.volume = 0.7f;
+        bgm_source.volume = 0.5f;
+
+        ToggleBGM(true);
+
+        //Attach Listeners to buttons for UI sounds
+        AttachListenersToButtons();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If background music is set to play and is not currently playing, play it.
+        if (bgm_playing == true && bgm_source.isPlaying == false)
+        {
+            bgm_source.Play();
+        }
+        else
+        {
+            //If background music is set to not play and is currently playing, stop it.
+            if (bgm_playing == false && bgm_source.isPlaying == true)
+            {
+                bgm_source.Stop();
+            }
+        }
+    }
+
+    // Sets the background music on or off.
+    // If the background music was on, turn it off, and vice versa.
+    public void ToggleBGM()
+    {
+        bgm_playing = !bgm_playing;
+    }
+
+    // Sets the background music on or off.
+    // True sets the background music to on, false sets it to off
+    public void ToggleBGM(bool on)
+    {
+        bgm_playing = on;
+    }
+
+    //Sets the current backgroud music usig the associated tag
+    public void SetBGM(string tag)
+    {
+        if (bgm_dict.ContainsKey(tag))
+        {
+            //No need to set if specified bgm is the same.
+            if (tag != current_bgm)
+            {
+                bgm_source.Stop();
+                bgm_source.clip = Resources.Load(bgm_dict[tag]) as AudioClip; ;
+                //Set current_bgm to avoid redundant loading
+                current_bgm = tag;
+            }
+        }
+        else
+        {
+            TagNotFound(tag);
+        }
+    }
+
+    // Set the current typing sound using the associated tag.
+    public void SetTypeSound(string tag)
+    {
+        if (voices.ContainsKey(tag))
+        {
+            type_source.clip = voices[tag];
+        }
+        else
+        {
+            TagNotFound(tag);
+        }
+    }
+
+    // Set the current sound played by ui
+    public void SetUISound(string tag)
+    {
+        if (sfx_dict.ContainsKey(tag))
+        {
+            //No need to set if specified bgm is the same.
+            if (tag != current_ui)
+            {
+                ui_source.clip = Resources.Load(sfx_dict[tag]) as AudioClip; 
+                //Set current_bgm to avoid redundant loading
+                current_ui = tag;
+            }
+        }
+        else
+        {
+            TagNotFound(tag);
+        }
+    }
+
+    // Plays the currently set typing sound.
+    public void PlayTyping()
+    {
+        type_source.Play();
+    }
+
+    // Plays a single sound effect as specified by the tag.
+    public void PlayOneSFX(string tag)
+    {
+        if (sfx_dict.ContainsKey(tag))
+        {
+            //No need to load sound effect if it was played last. 
+            if (current_sfx != tag)
+            {
+                AudioClip sfx = Resources.Load(sfx_dict[tag]) as AudioClip;
+                sfx_source.clip = sfx;
+                current_sfx = tag;
+            }
+            sfx_source.Play();
+        }
+        else
+        {
+            TagNotFound(tag);
+        }
+    }
+
+    // Sets the UI sound to "select" and plays it.
+
+    void PlaySelect()
+    {
+        SetUISound("select");
+        ui_source.Play();
+    }
+    
+
+    void AttachListenersToButtons()
+    {
+        GameObject canvas = GameObject.Find("Canvas");
+        Button[] buttons = canvas.GetComponentsInChildren<Button>(true);
+        for(int i = 0; i< buttons.Length; i++)
+        {
+            buttons[i].onClick.AddListener(PlaySelect);
+        }
+    }
+
+    void TagNotFound(string tag)
+    {
+        Debug.Log("Sound tag " + tag + " not found.");
+    }
+}
+
---
+++ Untitled Phoenix Wright/Assets/Scripts/AudioTestScripts/Test_Dialogue_Manager.cs
@@ -0,0 +1,268 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//This is a test version of the dialogue manager that calls methods from the audio manager for typing sounds.
+
+//After loading and processing dialogues from JsonLoader, go to dialogue manager script
+//This is the script that is in charge of "Phoenix Wright Style" text dialogue
+
+//Dialogue manager reads from a list of processed dialogue class, and plays them in type writer effect on Canvas UI
+public class Test_Dialogue_Manager : MonoBehaviour {
+
+
+    // ------Inspector Input-------
+    public GameObject panel;  //UI panel,which is where we should attach this script to. The reason to use a public inspector input is for potential deactivation
+    public Text name; //The UI text field that is going to display who's speaking
+    public Text conversation;///The UI text field that is going to display what are they saying
+
+    
+
+    //animation//protrait display
+    GameObject animation_display; 
+    Animator anim;
+
+
+    public GameObject Arrow;        // The little arrow thing to tell the player next dialogue
+   
+    public string section_call="null";     //making it publuc for debug purposes. This is which part of the dialogue we are calling.
+
+    //------ End of Inspector Input--------
+
+
+    // Dialogue diplayers  
+
+    public Dictionary<string, List<Type_Dialogue>> Script = 
+        new Dictionary<string, List<Type_Dialogue>>(); // Prepare a Script object that's dictionary composed from JsonLoader
+
+
+    int line_count = 0;                                         // a counter that keeps track of which line is playing
+    int end_of_chapter;                                         // an end indicator to tell object stop conversation
+    bool in_conversation = false;                               // determine whether the chat box disappears
+   
+
+   
+    ///
+    float wait_time = 0.1f; //how long per each letter
+    bool done = true; // is the current dialogue donw (are we calling arrow)
+    ///  
+  
+
+    ///All the Audio Crap in the world. Probably will move most of the audio to a separate audio manager
+
+    List<string> malevoices = new List<string>{ "???", "Miles" }; // a list of strings containing names of the male voices
+                                                                                   // the type writer can then play different noises accordingly
+    List<string> fremalevoices = new List<string> {};
+
+    //Can add more List<string> for more type noises
+
+    AudioManager audio_manager;
+    // audio objects
+    ///
+    void Start () {
+
+        //Script
+         GameObject loader = GameObject.FindGameObjectWithTag("Script_Data");  //find the xml loader that has the xml data in the scene
+         Script =  loader.GetComponent<LoadJson>().sections;       //Assgin the processed dialogues in xmlloder as Script
+        //panel.SetActive(false);                                    //Panel is initially in a resting state,comment out for debug purposes
+
+        //Audio
+        audio_manager = GameObject.FindGameObjectWithTag("Audio_Manager").GetComponent<AudioManager>();
+
+        //Art
+
+        Arrow.SetActive(false);     //shut the arrow
+
+        animation_display = GameObject.FindGameObjectWithTag("Character_Animator");
+        anim = animation_display.GetComponent<Animator>();
+
+    }
+
+    private void OnEnable() //everytime when the panel shows up,reset everything.
+    {
+        reset_dialogue_box();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+        if (section_call != "null")//if we know what we are talking
+        {
+
+        //    Debug.Log(section_call);
+            
+            end_of_chapter = Script[section_call].Count;    //Mark the end
+
+
+            if (done)//if one dialogue is over
+            {
+                Arrow.SetActive(true);//pop the arrow
+            }
+            if (line_count >= end_of_chapter && done == true)   // if we hit the end
+            {
+                in_conversation = false;
+            }
+            if (Input.GetMouseButtonDown(0) && in_conversation == false && done == true)
+            {
+                //section_call = "null";
+                panel.SetActive(false);
+                reset_dialogue_box();
+               
+            }
+            else if (line_count ==0)//if we are just starting, auto play one
+            {
+                forward_dialogue();
+            }
+            
+            else if (Input.GetMouseButtonDown(0) &&in_conversation)//if mouse click
+            {
+                forward_dialogue();
+            }
+        }
+
+	}
+
+    void reset_dialogue_box() //clear all contents
+    {
+        line_count = 0;
+        name.text = "";
+        conversation.text = "";
+        animation_display = GameObject.FindGameObjectWithTag("Character_Animator");
+        anim = animation_display.GetComponent<Animator>();
+        animation_display.GetComponent<Animator>().runtimeAnimatorController = Resources.Load<GameObject>("Arts/" + "Characters/null/null").GetComponent<Animator>().runtimeAnimatorController; 
+        animation_display.GetComponent<Image>().sprite = Resources.Load<GameObject>("Arts/" + "Characters/null/null").GetComponent<Image>().sprite;
+
+        done = true;
+        in_conversation = true;
+    }
+
+    void forward_dialogue() //move on to next line 
+    {
+      
+        //    Debug.Log("Current Index: " + line_count + " MaxCount: " + end_of_chapter);
+        if (!done)   //if we are not done, make it type faster and make sure the arrow is not on
+        {
+            wait_time = 0.04f;
+            Arrow.SetActive(false);
+        }
+        if (done) //if we are done, set wait time back to default. 
+        {
+            wait_time = 0.1f;
+            name.text = Script[section_call][line_count].name;
+            //play the current line out
+            string processing = Script[section_call][line_count].text; //make a string for the content
+                                                                       // Debug.Log(Script[line_count].icon);
+
+            
+            
+            GameObject animation_prefab = Resources.Load<GameObject>("Arts/" + "Characters/" + Script[section_call][line_count].character+ "/" + Script[section_call][line_count].animation);
+
+            
+//            Debug.Log(("Arts/" + "Characters/" + Script[section_call][line_count].character + "/" + Script[section_call][line_count].animation));
+
+            
+            animation_display.GetComponent<Animator>().runtimeAnimatorController = animation_prefab.GetComponent<Animator>().runtimeAnimatorController;
+            animation_display.GetComponent<Image>().sprite = animation_prefab.GetComponent<Image>().sprite;
+
+            StartCoroutine(PlayText(processing, conversation));//call Coroutine to type write
+            Arrow.SetActive(false);//shut the arrow
+            line_count++;//prepare for the next
+
+        }
+
+    }
+
+    IEnumerator PlayText(string story,Text conversation)  // Type Writer Coroutine (requires the string to type, and where to display)
+    {
+        done = false; //mark it as not done
+        conversation.text = ""; //clear the current chat box
+        string command_end = "</color>";//(Currently hardcoded) Dealing with rich text crap
+        string command_begin = ""; //^
+        bool color = false;//are we using rich text bs
+
+
+        //determine which sound is beeping, can add more if statements
+        if (malevoices.Contains(Script[section_call][line_count].name))
+        {
+            audio_manager.SetTypeSound("male");
+            //Testing sound effects, remove later.
+            audio_manager.PlayOneSFX("shock");
+        }
+        else if (Script[section_call][line_count].name == " ")
+        {
+            audio_manager.SetTypeSound("neutral");
+        }
+        else
+        {
+            audio_manager.SetTypeSound("female");
+        }
+    
+
+        string current; // current is the string we are about to type out
+
+        for (int i =0; i!= story.Length;i++) //for every letter in the dialogue
+        {
+            
+            if(story[i] == '<' && story[i+1] != '/')//if this is the beginning of a rich text, record the color
+            { 
+                command_begin = "";
+                command_begin += story[i];
+                int count = i + 1;
+                while(story[count]!='>')
+                {
+                    command_begin += story[count];
+                    count++;
+                }//getting the color code
+
+                command_begin += story[count];
+                color = true; //begin coloring returns
+                i = count + 1;
+            }
+
+            if (story[i] == '<' && story[i + 1] == '/') //if it is a </  sign
+            {
+                i++; 
+                int count =i;
+                while (story[count] !='>') //until you hit the end of the command, skip everything
+                {
+                    i++;
+                    count++;
+                }
+                
+                color = false;//not in color anymore
+            }
+
+
+            if (story[i] == '>')// if it hits > , skip
+                continue;
+
+            current = story[i].ToString();
+            if (color == true)  // if we are still in color mode, add color
+             {
+                string add = command_begin + story[i] + command_end;
+                conversation.text += add;
+          
+             }
+            else//if not, just add the text to yield string
+             {
+                conversation.text += story[i];
+                
+                
+               }
+
+
+            //Audio Management
+            if(current!= "\n")
+            {
+                audio_manager.PlayTyping();
+            }
+           
+            yield return new WaitForSeconds(wait_time); //type writer
+
+
+        }
+
+        done = true; // when we are done, mark done as true
+    }
+}
