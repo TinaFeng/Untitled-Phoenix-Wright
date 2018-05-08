f9bec4fed4f3674090df85060957381d151b6cb8
Lawrence Dang <reblank.et@gmail.com>
2018-05-08 14:09:04 -0700

    trying something, don't kill me

---
+++ Untitled Phoenix Wright/Assets/Scripts/SoundBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script is called by the Interactable script if it is attached to the same GameObject.
+//Loads the file with the supplied name as an AudioClip and plays it.
+
+public class SoundBehavior : MonoBehaviour, IInteractionBehavior {
+
+    AudioSource audioSource;
+    public string audio_name;
+	// Use this for initialization
+	void Start () {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = Resources.Load<AudioClip>(audio_name);
+	}
+	
+	public void PerformBehavior () {
+        audioSource.Play();
+	}
+}
