a3057f30d8e56cebde72caf22869a92b90654e73
Unknown <xuanfeng1996@gmail.com>
2018-06-05 17:38:52 -0700

    Fixed up UI and Updated prefab

--- Untitled Phoenix Wright/Assets/Scripts/Item_Button_Behavior.cs
+++ Untitled Phoenix Wright/Assets/Scripts/Item_Button_Behavior.cs
@@ -6,13 +6,5 @@ public class Item_Button_Behavior : MonoBehaviour {
 
     public string item_name;
     public string item_description;
-	// Use this for initialization
-	void Start () {
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
